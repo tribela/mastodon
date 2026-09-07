4158ecaefa5e0a2553b63fdac756eabcb7d94fe2
Echo <ChaosExAnima@users.noreply.github.com>
2026-09-07 15:46:09 +0000

    [Glitch] Status redesign: Fix padding and adds variants
    Port 834fcf0a844e43fa4906e5291cd7407a4e2a3ff3 to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/components/status/utils.ts
+++ app/javascript/flavours/glitch/components/status/utils.ts
@@ -1,11 +1,8 @@
-import type {
-  AccountStatusShape,
-  StatusShape,
-} from '@/flavours/glitch/models/status';
+import type { AnyStatusShape } from '@/flavours/glitch/models/status';
 
 export function statusLink({
   account,
   id,
-}: Pick<StatusShape | AccountStatusShape, 'account' | 'id'>) {
+}: Pick<AnyStatusShape, 'account' | 'id'>) {
   return `/@${typeof account === 'string' ? account : account.acct}/${id}`;
 }
