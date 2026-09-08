bdd326c5f662a086a207b94d86132399e39caf86
Echo <ChaosExAnima@users.noreply.github.com>
2026-09-08 13:54:16 +0000

    [Glitch] Status redesign: Quotes
    Port c5a69e523ec852ddb2de88feaa6fc7e3f272bb4d to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/selectors/filters.ts
+++ app/javascript/flavours/glitch/selectors/filters.ts
@@ -1,7 +1,6 @@
-import { createAppSelector } from 'flavours/glitch/store';
-import { toServerSideType } from 'flavours/glitch/utils/filters';
-
-import type { StatusContextType } from '../components/status/types';
+import type { StatusContextType } from '@/flavours/glitch/components/status/types';
+import { createAppSelector } from '@/flavours/glitch/store/typed_functions';
+import { toServerSideType } from '@/flavours/glitch/utils/filters';
 
 import { selectExpandedStatus } from './statuses';
 
@@ -74,34 +73,6 @@ export const selectStatusFilters = createAppSelector(
     }
 
     return results;
-  },
-);
-
-export const selectStatusLoadingState = createAppSelector(
-  [
-    (state, { statusId }: { statusId?: string | null }) =>
-      selectExpandedStatus(state, statusId ?? undefined),
-    selectStatusFilters,
-    (_, { warnInsteadOfHide }: { warnInsteadOfHide?: boolean }) =>
-      warnInsteadOfHide,
-  ],
-  (status, filters, warnInsteadOfHide) => {
-    if (!status) {
-      return { state: 'not-found', status: null };
-    }
-
-    if (status.isLoading) {
-      return { state: 'loading', status: null };
-    }
-
-    if (
-      !warnInsteadOfHide &&
-      filters.some((filter) => filter.filter_action === 'hide')
-    ) {
-      return { state: 'filtered', status: null };
-    }
-
-    return { state: 'loaded', status };
   },
 );
 
