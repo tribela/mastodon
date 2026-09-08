6908d06b871bbed307b2f7b45aa97d03140e84d4
diondiondion <mail@diondiondion.com>
2026-09-08 16:50:48 +0000

    [Glitch] Redesign: Make navigation sections collapsible
    Port 26fce0f2f9e36ea0e0c2b03b7f57d1b1ea58ed1c to glitch-soc
    
    Signed-off-by: Claire <claire.github-309c@sitedethib.com>

--- app/javascript/flavours/glitch/features/navigation_panel/redesign/list_section.tsx
+++ app/javascript/flavours/glitch/features/navigation_panel/redesign/list_section.tsx
@@ -1,7 +1,15 @@
 import type { ReactNode } from 'react';
 
-import { Button } from '@/flavours/glitch/components/button/redesign';
+import { FormattedMessage } from 'react-intl';
+
+import { CaretDownIcon } from '@phosphor-icons/react';
+
+import {
+  Button,
+  IconButton,
+} from '@/flavours/glitch/components/button/redesign';
 import type { MastodonLocationDescriptor } from '@/flavours/glitch/components/router';
+import { useToggle } from '@/flavours/glitch/hooks/useToggle';
 import { hasReactChildren } from '@/flavours/glitch/utils/has_react_children';
 
 import classes from './list_section.module.scss';
@@ -16,10 +24,28 @@ export const ListSection: React.FC<{
   emptyMessage?: ReactNode;
 }> = ({ title, action, children, emptyMessage }) => {
   const hasContent = hasReactChildren(children);
+  const [isOpen, { onToggle }] = useToggle(true);
 
   return (
     <li className={classes.root}>
       <div className={classes.titleWrapper}>
+        {hasContent && (
+          <IconButton
+            size='sm'
+            variant='ghost'
+            icon={CaretDownIcon}
+            onClick={onToggle}
+            noActiveHighlight
+            aria-expanded={isOpen}
+            className={classes.toggleButton}
+          >
+            <FormattedMessage
+              id='tabs_bar.open_section'
+              defaultMessage='Open {title} menu'
+              values={{ title }}
+            />
+          </IconButton>
+        )}
         <span className={classes.title}>{title}</span>
         {action && (
           <Button
@@ -32,11 +58,11 @@ export const ListSection: React.FC<{
           </Button>
         )}
       </div>
-      {hasContent ? (
-        <ul>{children}</ul>
-      ) : (
-        emptyMessage && <div className={classes.emptyState}>{emptyMessage}</div>
-      )}
+      {hasContent
+        ? isOpen && <ul>{children}</ul>
+        : emptyMessage && (
+            <div className={classes.emptyState}>{emptyMessage}</div>
+          )}
     </li>
   );
 };
